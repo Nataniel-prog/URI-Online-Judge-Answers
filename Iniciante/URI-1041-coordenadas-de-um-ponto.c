a5f7a6135a9e7a92f71d23218c65dbdfae654dbb
Nataniel-prog <53264728+Nataniel-prog@users.noreply.github.com>
2019-08-01 09:36:54 -0300

    Update URI-1041-coordenadas-de-um-ponto.c

--- Iniciante/URI-1041-coordenadas-de-um-ponto.c
+++ Iniciante/URI-1041-coordenadas-de-um-ponto.c
@@ -1,33 +1,38 @@
-#include <stdio.h>
-int main (){
-	
-	float x, y;
+#include<stdio.h>
 
-	scanf("%f %f", &x, &y);
-	
-	if(x == 0.0 && y == 0.0){ 
-		printf("Origem\n");
-	}
-	else if (x > 0 && y > 0){
-		printf("Q1\n");
-	}
-	else if (x < 0 && y > 0){
-		printf("Q2\n");
-	}
-	else if (x < 0 && y < 0){
-		printf("Q3\n");
-	}
-	else if (x > 0 && y < 0){
-		printf("Q4\n");
-	}
-	else if (x == 0 && y > 0 || y < 0){
-		printf("Eixo Y\n");
-	}
-	else if (x > 0 || x < 0 && y == 0)
-	{
-		printf("Eixo X\n");	
-	}
+/*
+Leia 2 valores com uma casa decimal(x e y)
+Determine o quadrante ao qual pertence o ponto
+Ou se está sobre um dos eixos cartesianos ou na origem(x=y=0)
+Se o ponto estiver na origem, escrever a mensagem "Origem".
+Se o ponto estiver sobre um dos eixos escreva "Eixo X" ou "Eixo y"
+*/
 
-	return 0;
+int main(){
+    float x,y;
 
+    scanf("%f%f",&x,&y);
+
+    if(x==0.0 && y==0.0){
+        printf("Origem\n");
+    }
+    else if(x>0 && y>0){
+        printf("Q1\n");
+    }
+    else if(x<0 && y>0){
+        printf("Q2\n");
+    }
+    else if(x<0 && y<0){
+        printf("Q3\n");
+    }
+    else if(x>0 && y<0){
+        printf("Q4\n");
+    }
+    else if(x==0 && y>0 || y<0){
+        printf("Eixo Y\n");
+    }
+    else if(x>0 || x<0 && y==0){
+        printf("Eixo X\n");
+    }
+    return 0;
 }
